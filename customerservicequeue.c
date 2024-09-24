614dc47c5ed98cfbcbe96be05ad1182652678f35
Payaldebug <2023pcecspayal113@poornima.org>
2024-09-24 09:57:51 +0530

    Add files via upload

---
+++ customerservicequeue.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#define MAXSIZE 10
+int queue[MAXSIZE];
+int rear=-1;
+int front=0;
+
+void Enqueue()
+{ 
+    int id;
+    if(rear==MAXSIZE-1)
+    {
+        printf("\nCustomer service is Full");
+    }
+    else{
+        printf("\nEnter your customer id");
+        scanf("%d",&id);
+        rear++;
+        queue[rear]=id;
+    }
+}
+
+void Dequeue()
+{
+    int id;
+    if(front>rear)
+    {
+        printf("\nCustomer Service is Empty");
+    }
+    else{
+        id=queue[rear];
+        front++;
+        printf("Delected customer%d",id);
+        }
+}
+
+void Peek()
+{
+    if(front>rear)
+    {
+        printf("\nCustomer Service is Empty");
+    }
+    else{
+        printf("View the most recent customer%d",queue[rear]);
+    }
+}
+
+void Display()
+{
+    if(front>rear)
+        {
+        printf("\nCustomer Service is Empty");
+    }
+    else{
+        int i;
+          printf("All Customer are Show");
+          for(i=rear;i>=front;i--)
+          printf("%d",queue[i]);
+}
+}
+int main()
+{   
+    int choice;
+    do{
+        printf("\nWelcome to the Customer service!");
+        printf("\n1.Enqueue\n2.Dequeue\n3.Peek\n4.Display");
+        printf("\nEnter your choice");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+           case 1:Enqueue();
+           break;
+           case 2:Dequeue();
+           break;
+           case 3:Peek();
+           break;
+           case 4:Display();
+           break;
+           default:
+           printf("invalid choice!");
+        }
+    }while(choice!=5);
+    return 0;
+}
