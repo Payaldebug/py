4599df0fdf4d3b313351f7f68aa48e81be4e2d9a
Payaldebug <2023pcecspayal113@poornima.org>
2024-09-26 19:43:35 +0530

    Add files via upload

---
+++ towerhanoi.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void tower(int n, char beg, char aux, char end)
+{
+    if(n<=0)
+    {
+        printf("not possible");
+    }
+    else if(n==1)
+    {
+        printf("\n move disc from %c to %c",beg,end);
+    }
+    else{
+        tower(n-1,beg,end,aux);
+        tower(1,beg,aux,end);
+        tower(n-1,aux,beg,end);
+    }
+}
+
+int main()
+{
+    int n;
+    char a,b,c;
+    printf("Enter the number of disc\n");
+    scanf("%d",&n);
+    tower(n,'a','b','c');
+    return 0;
+}
